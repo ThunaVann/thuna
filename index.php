c23b4a72aca39ce6b9043723dea7141f8459ff2e
ThunaVann <45807139+ThunaVann@users.noreply.github.com>
2018-12-11 18:57:27 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+<!--
+	uBeasa by freshdesignweb.com
+	Twitter: https://twitter.com/freshdesignweb
+	URL: https://www.freshdesignweb.com
+-->
+<html>
+	<?php
+	include_once('vs/head.php');
+	?>
+	<body>
+		
+	<div class="ubea-loader"></div>
+<!--start header-->
+	<?php
+	include_once('vs/header.php');
+	?>
+<!--end header-->
+
+<!--start slider-->
+	<?php
+	include_once('vs/slider.php');
+	?>
+<!--end slider-->
+
+<!--start service-->
+	<?php
+		include_once('vs/service.php');
+	?>
+		</div>
+<!--end service-->
+
+<!--start Portfolio-->
+	<?php
+		include_once('vs/Portfolio.php');
+	?>
+<!--end Portfolio-->
+<?php
+		include_once('vs/vilalangka.php');
+	?>
+	<?php
+		include_once('vs/wine.php');
+	?>
+<!--start Frequently Ask Questions-->    
+	<?php
+		include_once('vs/frequently_ask_questions.php');
+	?>
+	</div>
+<!--end  Frequently Ask Questions-->
+	<!--start About us-->
+    
+   <?php
+		include_once('vs/about_us.php');
+	?>
+	<!--end About us-->
+   <!--start slider1-->
+   <?php
+		include_once('vs/slider1.php');
+	?>
+   <!--end slider1-->
+   
+<?php
+		include_once('vs/footer.php');
+	?>
+	
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	<!--
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!--<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>-->
+	
+
+	</body>
+</html>
+
